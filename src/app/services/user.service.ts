533a61098dad99924dc91b91bdd97a6ef12d794b
eckolosst <lucasp.1606@gmail.com>
2017-10-20 13:34:43 -0300

    Comenzando con el uso de tokens

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,41 +1,55 @@
 import { Injectable } from '@angular/core';
-import { UserComponent } from "../user/user.component";
-import { Router } from '@angular/router';
-
-// Provisional, en un futuro iría en la BD
-var users = [
-  new UserComponent('admin','admin'),
-  new UserComponent('admin1','admin1')
-];
+import { Http, Response, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+import { GLOBAL } from '../global';
 
 @Injectable()
 export class UserService {
+    public url: String;
+    public identity;
+    public token;
 
-constructor(private _router: Router) { }
-
-
-/*---------------------------------------------------------------------------*/
-/*                        Peticiones para Usuarios                           */
-/*---------------------------------------------------------------------------*/
-
-  logout() {
-    localStorage.removeItem("user");
-    this._router.navigate(['/login']);
-  }
-
-  login(user) {
-    let authenticatedUser = users.find(u => u.email === user.username);
-    if (authenticatedUser && authenticatedUser.password === user.password){
-      localStorage.setItem("user", authenticatedUser.email);
-      this._router.navigate(['/home']);
-      return true;
+    constructor(private _http:Http){
+        this.url = GLOBAL.url;
     }
-    return false;
-  }
 
-  checkCredentials() {
-    if (localStorage.getItem("user") === null){
-      this._router.navigate(['/login']);
+    register(user_to_register){
+        let params = JSON.stringify(user_to_register);
+        let headers = new Headers({'Content-Type': 'application/json'});
+
+        return this._http.post(this.url+'/registro/', params, {headers:headers})
+                         .map(res => res.json());
     }
-  }
+
+    singup(user_to_login, gettoken = null){
+        if(gettoken != null){
+            user_to_login.gettoken = gettoken;
+        }
+        let params = JSON.stringify(user_to_login);
+        let headers =  new Headers({'Content-Type': 'application/json'});
+
+        return  this._http.post(this.url+'/usuarioLog/', params, {headers:headers})
+                          .map(res => res.json());
+    }
+
+    getIdentity(){
+        let identity = JSON.parse(localStorage.getItem('identity'));
+        if(identity != 'undefined'){
+            this.identity = identity;
+        }else{
+            this.identity = null;
+        }
+        return this.identity;
+    }
+
+    getTokem(){
+        let token = localStorage.getItem('token');
+        if(token != 'undefined'){
+            this.token = token;
+        }else{
+            this.token = null;
+        }
+        return this.token
+    }
 }
